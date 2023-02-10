d5c29e175b7820090913226313d04d7f3340d1e0
CalaW <maker_cc@foxmail.com>
2023-02-11 01:09:27 +0800

    update to fontawesome 6.3.0

---
+++ link_to_texmf.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# This script is used to link package in this directory to the user's texmf directory
+
+ln -s ~/Documents/Program/latex-fontawesome6/fontawesome6.sty ~/Library/texmf/tex/latex/fontawesome6/fontawesome6.sty
