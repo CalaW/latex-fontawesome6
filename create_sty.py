627c10a98271c7f7dff20f998ffcf7330ee21fb2
CalaW <maker_cc@foxmail.com>
2024-11-20 12:27:47 +0800

    6.7.0

--- create_sty.py
+++ create_sty.py
@@ -15,7 +15,7 @@ OUTPUT_HEADER = (
 \NeedsTeXFormat{LaTeX2e}
 \ProvidesPackage{fontawesome6}["""
     + date.today().strftime("%Y/%m/%d")
-    + R""" v6.6.0 font awesome icons]
+    + R""" v6.7.0 font awesome icons]
 
 % Requirements to use.
 \RequirePackage{fontspec}
